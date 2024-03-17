66f7fef5e0aa9c7187a69452b3e5442a6e7f906e
Dmitry Suzdalev <mail@dimsuz.ru>
2024-03-17 22:22:35 +0200

    Finished demo1

--- android/src/ru/dimsuz/libgdx/test/AndroidLauncher.java
+++ android/src/ru/dimsuz/libgdx/test/AndroidLauncher.java
@@ -1,6 +1,9 @@
 package ru.dimsuz.libgdx.test;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -10,7 +13,19 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGdxGame(), config);
+		View renderView = initializeForView(new MyGdxGame(), config);
+		FrameLayout layout = findViewById(R.id.frame);
+		layout.addView(renderView, 0);
+
+		findViewById(R.id.button).setOnClickListener(view -> {
+			((Button)view).setText("Clicked!");
+			getHandler().postDelayed(() -> ((Button)view).setText("Button"), 800);
+		});
+		findViewById(R.id.smol_button).setOnClickListener(view -> {
+			((Button)view).setText("Clicked!");
+			getHandler().postDelayed(() -> ((Button)view).setText("Button"), 800);
+		});
 	}
 }
--- core/src/ru/dimsuz/libgdx/test/MyGdxGame.kt
+++ core/src/ru/dimsuz/libgdx/test/MyGdxGame.kt
@@ -2,32 +2,47 @@ package ru.dimsuz.libgdx.test
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.Input
-import com.badlogic.gdx.Input.Keys.O
-import com.badlogic.gdx.InputProcessor
-import com.badlogic.gdx.graphics.Camera
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.input.GestureDetector
+import com.badlogic.gdx.maps.MapLayer
 import com.badlogic.gdx.maps.tiled.TiledMap
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer
 import com.badlogic.gdx.maps.tiled.TmxMapLoader
+import com.badlogic.gdx.maps.tiled.objects.TiledMapTileMapObject
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
 import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.utils.ScreenUtils
 
 class MyGdxGame : ApplicationAdapter(), GestureDetector.GestureListener {
   private lateinit var camera: OrthographicCamera
   private lateinit var tiledMap: TiledMap
   private lateinit var tiledMapRenderer: TiledMapRenderer
+  private var mapTop: Float = 0f
+  private lateinit var bgLayer: TiledMapTileLayer
+  private lateinit var kiosksLayer: MapLayer
+
+  private var kiosksActive: BooleanArray = BooleanArray(0)
+  private var kiosksAnimateActive: BooleanArray = BooleanArray(0)
+  private var kiosksOpacity: FloatArray = FloatArray(0)
+  private var kiosksIds: IntArray = IntArray(0)
 
   override fun create() {
     camera = OrthographicCamera()
-    camera.setToOrtho(true, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
-    camera.update()
+    camera.setToOrtho(false, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
     tiledMap = TmxMapLoader().load("ready.tmx")
-    tiledMapRenderer = OrthogonalTiledMapRenderer(tiledMap)
+    tiledMapRenderer = OrthogonalTiledMapRendererWithObjects(tiledMap)
+    bgLayer = tiledMap.layers.get(0) as TiledMapTileLayer
+    kiosksLayer = tiledMap.layers.get(1)
+    kiosksActive = BooleanArray(kiosksLayer.objects.count)
+    kiosksAnimateActive = BooleanArray(kiosksLayer.objects.count)
+    kiosksOpacity = FloatArray(kiosksLayer.objects.count) { 0.2f }
+    kiosksIds = IntArray(kiosksLayer.objects.count) { kiosksLayer.objects.get(it).properties.get("id") as Int }
+    mapTop = bgLayer.height * bgLayer.tileHeight.toFloat()
+    camera.position.y = mapTop - camera.viewportHeight / 2
+    camera.update()
     Gdx.input.inputProcessor = GestureDetector(this)
   }
 
@@ -35,10 +50,24 @@ class MyGdxGame : ApplicationAdapter(), GestureDetector.GestureListener {
     ScreenUtils.clear(1f, 0f, 0f, 1f)
     camera.update()
     tiledMapRenderer.setView(camera)
+
+    for (i in kiosksAnimateActive.indices) {
+      if (kiosksAnimateActive[i]) {
+        val durationSecs = 0.3f
+        kiosksOpacity[i] = (kiosksOpacity[i] + Gdx.graphics.deltaTime * 0.8f / durationSecs)
+        if (kiosksOpacity[i] >= 1f) {
+          kiosksOpacity[i] = 1f
+          kiosksActive[i] = true
+          kiosksAnimateActive[i] = false
+        }
+      }
+    }
+
     tiledMapRenderer.render()
   }
 
   override fun dispose() {
+    tiledMap.dispose()
   }
 
   override fun touchDown(x: Float, y: Float, pointer: Int, button: Int): Boolean {
@@ -46,6 +75,15 @@ class MyGdxGame : ApplicationAdapter(), GestureDetector.GestureListener {
   }
 
   override fun tap(x: Float, y: Float, count: Int, button: Int): Boolean {
+    for ((index, obj) in kiosksLayer.objects.withIndex()) {
+      val tapCoord = camera.unproject(Vector3(x, y, 0f))
+      val o = obj as TiledMapTileMapObject
+      if (tapCoord.x >= o.x && tapCoord.x < o.x + o.tile.textureRegion.regionWidth &&
+        tapCoord.y >= o.y && tapCoord.y < o.y + o.tile.textureRegion.regionHeight) {
+        kiosksActive[index] = true
+        kiosksAnimateActive[index] = true
+      }
+    }
     return false
   }
 
@@ -58,12 +96,12 @@ class MyGdxGame : ApplicationAdapter(), GestureDetector.GestureListener {
   }
 
   override fun pan(x: Float, y: Float, deltaX: Float, deltaY: Float): Boolean {
-    camera.translate(-deltaX, -deltaY)
+    camera.translate(-deltaX, deltaY)
     if (camera.position.x < camera.viewportWidth / 2) {
       camera.position.x = camera.viewportWidth / 2
     }
-    if (camera.position.y < camera.viewportHeight / 2) {
-      camera.position.y = camera.viewportHeight / 2
+    if (camera.position.y > mapTop - camera.viewportHeight / 2) {
+      camera.position.y = mapTop - camera.viewportHeight / 2
     }
     return false
   }
@@ -88,4 +126,19 @@ class MyGdxGame : ApplicationAdapter(), GestureDetector.GestureListener {
   override fun pinchStop() {
   }
 
+  private inner class OrthogonalTiledMapRendererWithObjects(map: TiledMap) : OrthogonalTiledMapRenderer(map) {
+
+    override fun renderObjects(layer: MapLayer) {
+      for ((index, obj) in layer.objects.withIndex()) {
+        if (obj is TiledMapTileMapObject) {
+          val tile = obj.tile
+          val color = batch.color.cpy()
+          batch.setColor(color.r, color.g, color.b, kiosksOpacity[index])
+          batch.draw(tile.textureRegion, obj.x, obj.y)
+          batch.color = color
+        }
+      }
+    }
+  }
 }
+
