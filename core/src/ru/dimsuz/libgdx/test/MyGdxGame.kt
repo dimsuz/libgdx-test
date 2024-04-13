ca69ed87b4be02d675dac5ea15602924b64fa788
Dmitry Suzdalev <mail@dimsuz.ru>
2024-04-13 19:22:44 +0200

    Add pan limiters logic

--- core/src/ru/dimsuz/libgdx/test/MyGdxGame.kt
+++ core/src/ru/dimsuz/libgdx/test/MyGdxGame.kt
@@ -4,9 +4,11 @@ import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.InputMultiplexer
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.input.GestureDetector
 import com.badlogic.gdx.maps.tiled.TiledMap
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer
@@ -14,6 +16,7 @@ import com.badlogic.gdx.maps.tiled.TmxMapLoader
 import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer
 import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.utils.ScreenUtils
 import com.badlogic.gdx.utils.viewport.ExtendViewport
 import com.badlogic.gdx.utils.viewport.Viewport
@@ -26,6 +29,8 @@ class MyGdxGame : ApplicationAdapter(), GestureDetector.GestureListener {
   private lateinit var batch: SpriteBatch
   private lateinit var font: BitmapFont
   private var debugToolsVisible = true
+  private var tmpVector3 = Vector3()
+  private lateinit var debugShapes: ShapeRenderer
 
   private var mapWidthTiles: Int = 0
   private var mapHeightTiles: Int = 0
@@ -37,6 +42,8 @@ class MyGdxGame : ApplicationAdapter(), GestureDetector.GestureListener {
   override fun create() {
     batch = SpriteBatch()
     font = BitmapFont()
+    debugShapes = ShapeRenderer()
+    debugShapes.color = Color.RED
 
     tiledMap = TmxMapLoader().load("aplCityProto1.tmx")
     tiledMapRenderer = IsometricTiledMapRenderer(tiledMap)
@@ -51,12 +58,20 @@ class MyGdxGame : ApplicationAdapter(), GestureDetector.GestureListener {
     mapHeightTiles = baseLayer.height
     tileWidth = baseLayer.tileWidth
     tileHeight = baseLayer.tileHeight
-    mapViewport = ExtendViewport(baseLayer.width.toFloat() * baseLayer.tileWidth, baseLayer.height.toFloat() * baseLayer.tileHeight, camera)
+    mapViewport = ExtendViewport(
+      baseLayer.width.toFloat() * baseLayer.tileWidth,
+      baseLayer.height.toFloat() * baseLayer.tileHeight,
+      camera
+    )
   }
 
   inner class MouseWheelScrollDetector : InputAdapter() {
     override fun scrolled(amountX: Float, amountY: Float): Boolean {
-      camera.zoom = MathUtils.clamp(camera.zoom + amountY * MOUSE_WHEEL_ZOOM_FACTOR, defaultZoom * (MAX_ZOOM_FACTOR - 1), defaultZoom)
+      camera.zoom = MathUtils.clamp(
+        camera.zoom + amountY * MOUSE_WHEEL_ZOOM_FACTOR,
+        defaultZoom - (defaultZoom * MAX_ZOOM_FACTOR - defaultZoom),
+        defaultZoom
+      )
       camera.update()
       return false
     }
@@ -68,10 +83,20 @@ class MyGdxGame : ApplicationAdapter(), GestureDetector.GestureListener {
     tiledMapRenderer.render()
     if (debugToolsVisible) {
       batch.begin()
-      font.draw(batch, "Tile size: ${tileWidth}x${tileHeight}", 16f, 16f + font.lineHeight * 3)
-      font.draw(batch, "MapSize: ${mapWidthTiles}x${mapHeightTiles}", 16f, 16f + font.lineHeight * 2)
+      font.draw(batch, "Pan: xmin=${panMinX}, xmax=${panMaxX})", 16f, 16f + font.lineHeight * 5)
+      font.draw(batch, "Pan: ymin=${panMinY}, ymax=${panMaxY})", 16f, 16f + font.lineHeight * 4)
+      font.draw(batch, "Camvport: (${camera.position.x - camera.viewportWidth/2f}, ${camera.position.y + camera.viewportHeight/2f})", 16f, 16f + font.lineHeight * 3)
+      font.draw(batch, "Camera: (${camera.position.x}, ${camera.position.y})", 16f, 16f + font.lineHeight * 2)
       font.draw(batch, String.format("Zoom: %.3f", camera.zoom), 16f, 16f + font.lineHeight)
       batch.end()
+
+      debugShapes.projectionMatrix = camera.combined
+      debugShapes.begin(ShapeRenderer.ShapeType.Line)
+      debugShapes.line(0f, panMinY, mapViewport.worldWidth, panMinY)
+      debugShapes.end()
+      debugShapes.begin(ShapeRenderer.ShapeType.Line)
+      debugShapes.line(0f, panMaxY, mapViewport.worldWidth, panMaxY)
+      debugShapes.end()
     }
   }
 
@@ -79,16 +104,31 @@ class MyGdxGame : ApplicationAdapter(), GestureDetector.GestureListener {
     tiledMap.dispose()
     batch.dispose()
     font.dispose()
+    debugShapes.dispose()
   }
+
+  private var isFirstResize = true
+  private var panMinX = 0f
+  private var panMaxX = 0f
+  private var panMinY = 0f
+  private var panMaxY = 0f
 
   override fun resize(width: Int, height: Int) {
     mapViewport.update(width, height, false)
-    camera.position.set(mapViewport.worldWidth/2f, 0f, 0f)
-    if (defaultZoom == 1f) {
+    camera.position.set(mapViewport.worldWidth / 2f, 0f, 0f)
+
+    if (isFirstResize) {
+      panMinX = mapViewport.worldWidth / 2f - (ACTIVE_MAP_SIZE_TILES * tileWidth) / 2f
+      panMaxX = mapViewport.worldWidth / 2f + (ACTIVE_MAP_SIZE_TILES * tileWidth) / 2f
+      panMinY = -(ACTIVE_MAP_SIZE_TILES * tileHeight) / 2f
+      panMaxY = (ACTIVE_MAP_SIZE_TILES * tileHeight) / 2f
       defaultZoom = ACTIVE_MAP_SIZE_TILES / mapWidthTiles.toFloat()
       camera.zoom = defaultZoom
     }
+
     camera.update()
+
+    isFirstResize = false
   }
 
   override fun touchDown(x: Float, y: Float, pointer: Int, button: Int): Boolean {
@@ -108,8 +148,21 @@ class MyGdxGame : ApplicationAdapter(), GestureDetector.GestureListener {
   }
 
   override fun pan(x: Float, y: Float, deltaX: Float, deltaY: Float): Boolean {
-    mapViewport.camera.translate(-deltaX, deltaY, 0f)
-    mapViewport.camera.update()
+    val halfViewportHeight = camera.viewportHeight * camera.zoom * 0.5f
+    val halfViewportWidth = camera.viewportWidth * camera.zoom * 0.5f
+    tmpVector3.set(camera.position)
+    tmpVector3.add(-deltaX, deltaY, 0f)
+    // limit panning only if both limit boundaries are outside of the camera's current view, otherwise
+    // we'd end up trying to clamp to the boundaries which are already *within* view range, this would require
+    // changing zoom
+    if (panMaxX - panMinX >= halfViewportWidth * 2) {
+      tmpVector3.x = MathUtils.clamp(tmpVector3.x, panMinX + halfViewportWidth, panMaxX - halfViewportWidth)
+    }
+    if (panMaxY - panMinY >= halfViewportHeight * 2) {
+      tmpVector3.y = MathUtils.clamp(tmpVector3.y, panMinY + halfViewportHeight, panMaxY - halfViewportHeight)
+    }
+    camera.position.set(tmpVector3)
+    camera.update()
     return false
   }
 
